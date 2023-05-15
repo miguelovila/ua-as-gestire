adb1fb10179ce8f17bd6bf55d4dc5a176259c0f1
Miguel Vila <miguelovila@ua.pt>
2023-05-15 01:09:53 +0100

    Basic login functionality

--- identity-provider/app.py
+++ identity-provider/app.py
@@ -5,8 +5,16 @@ from flask import Flask, current_app, render_template, request, redirect, url_fo
 
 app = Flask(__name__)
 
-@app.route('/idp/profile/saml2/redirect/sso', methods=['GET'])
+@app.route('/idp/profile/saml2/redirect/sso', methods=['GET', 'POST'])
 def loginPage():
+    if request.method == 'POST':
+        username = request.form["gestire_username"]
+        password = request.form["gestire_password"]
+        if len(executor("SELECT * FROM users WHERE email = ? AND password = ?;", (username, password))) > 0:
+            return redirect('https://google.com')
+        else:
+            return render_template('login.html', error="Invalid credentials.")
+
     return render_template('login.html')
 
 if __name__ == '__main__':
