61caed7477e686fb4e1aad9d2f65aa36dc131ac4
Miguel Vila <miguelovila@ua.pt>
2023-05-24 18:55:51 +0100

    Get equipments

--- backend/app.py
+++ backend/app.py
@@ -152,6 +152,66 @@ def getRoom(room_id):
             }
         ),400
     
+@app.route('/api/equipments', methods=['GET'])
+def listEquipments():
+    try:
+        content = json.loads(request.data)
+        if not checkToken(content['token']):
+            return json.dumps(
+                {
+                  "error": "Access denied"
+                }
+            ),401
+        
+        equipments = executor("SELECT * FROM equipments;")
+        if len(equipments) > 0:
+            return json.dumps(
+                {
+                  "equipments": equipments
+                }
+            ), 200
+        return json.dumps(
+            {
+              "error": "No equipments found"
+            }
+        ),400
+    except:
+        return json.dumps(
+            {
+              "error": "Invalid request"
+            }
+        ),400
+    
+@app.route('/api/equipments/<int:equipment_id>', methods=['GET'])
+def getEquipment(equipment_id):
+    try:
+        content = json.loads(request.data)
+        if not checkToken(content['token']):
+            return json.dumps(
+                {
+                  "error": "Access denied"
+                }
+            ),401
+
+        equipment = executor("SELECT * FROM equipments WHERE id = ?;", (equipment_id,))
+        if len(equipment) > 0:
+            return json.dumps(
+                {
+                  "equipment": equipment[0]
+                }
+            ), 200
+        return json.dumps(
+            {
+              "error": "Equipment not found"
+            }
+        ),400
+    except:
+        return json.dumps(
+            {
+              "error": "Invalid request"
+            }
+        ),400
+    
 if __name__ == '__main__':
     initializeDatabase()
     fillSampleUserData()
